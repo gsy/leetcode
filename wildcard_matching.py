a2e8639abe3fc49fead842fa0000722dac166366
xuanguang.chen <gtcxg@hotmail.com>
2015-12-20 22:35:51 +0800

    wildcard TLE...

--- wildcard_matching.py
+++ wildcard_matching.py
@@ -1,45 +1,83 @@
 __author__ = 'guang'
 
 class Solution(object):
-    def candidates(self, p):
+    def transitions(self, p):
         """
-
         :param p:
         :return:
         >>> s = Solution()
-        >>> s.candidates("aa")
-        ['aa']
-        >>> s.candidates("?*a")
-        ['?*a']
-        >>> s.candidates("*a")
-        ['a', '?*a']
-        >>> s.candidates("*")
-        ['', '?*']
+        >>> s.transitions("aa")
+        [{'a': 1}, {'a': 2}]
+        >>> s.transitions("a")
+        [{'a': 1}]
+        >>> s.transitions("?a")
+        [{'?': 1}, {'a': 2}]
+        >>> s.transitions("*")
+        [{'?': 0}]
+        >>> s.transitions("a*")
+        [{'a': 1}, {'?': 1}]
+        >>> s.transitions("c*a*b")
+        [{'c': 1}, {'a': 2, '?': 1}, {'b': 3, '?': 2}]
         """
-        if p[0] == '*':
-            return [p[1:], "?*" + p[1:]]
-        else:
-            return [p]
-
-    def reduce(self, p):
-        """
-
-        :param p:
-        :return:
-        >>> s = Solution()
-        >>> s.reduce("aa")
-        'aa'
-        >>> s.reduce('')
-        ''
-        """
-        return ''.join([x for x in p if x != '*'])
-
-    def patterns(self, p):
         result = []
-        for pattern in p.split("*"):
-            if pattern != '' and '?' not in pattern:
-                result.append(pattern)
+        state = 0
+        length = len(p)
+        for index, x in enumerate(p):
+            mapping = {}
+            state += 1
+            if x == '*':
+                mapping['?'] = state - 1
+                if index < length - 1:
+                    mapping[p[index+1]] = state
+            else:
+                if index > 0 and p[index - 1] == '*':
+                    state -= 1
+                    continue
+                else:
+                    mapping[x] = state
+            result.append(mapping)
 
+        return result
+
+    def accepting_state(self, transitions):
+        mapping = transitions[-1]
+        return mapping.values()
+
+    def judge(self, state, s, transitions):
+        if len(s) == 0:
+            return state in self.accepting_state(transitions)
+        elif state >= len(transitions):
+            return False
+
+        mapping = transitions[state]
+        possibles = mapping.keys()
+        x = s[0]
+
+        if x in possibles:
+            next_state = mapping[x]
+            if self.judge(next_state, s[1:], transitions):
+                return True
+        if '?' in possibles:
+            next_state = mapping['?']
+            if self.judge(next_state, s[1:], transitions):
+                return True
+            elif next_state != state:
+                return self.judge(next_state, s, transitions)
+
+        return False
+
+    def optimize(self, p):
+        if len(p) == 0:
+            return []
+
+        ps = p.split('*')
+        qs = [x for x in ps if x != '']
+        q = '*'.join(qs)
+        result = q
+        if p[0] == '*':
+            result = "*" + result
+        if len(p) > 1 and p[-1] == '*':
+            result = result + "*"
         return result
 
     def isMatch(self, s, p):
@@ -48,44 +86,28 @@ class Solution(object):
         :type p: str
         :rtype: bool
         >>> s = Solution()
+        >>> s.isMatch("cab", "c*a*b")
+        True
+        >>> s.isMatch("cccab", "c*a*b")
+        True
         >>> s.isMatch("aa", "a")
         False
-        >>> s.isMatch("aa", "aa")
-        True
-        >>> s.isMatch("aaa", "aa")
-        False
-        >>> s.isMatch("aaa", "*")
-        True
-        >>> s.isMatch("aa", "a*")
-        True
-        >>> s.isMatch("ab", "?*")
-        True
         >>> s.isMatch("aab", "c*a*b")
         False
-        >>> s.isMatch("ba", "?*a")
+        >>> s.isMatch("caaaabb", "c*a*b")
         True
-        >>> s.isMatch("bca", "??*a")
+        >>> s.isMatch("baabbabababaabbabababbaabbbbaaabaaabbbbaaaaaabbbbaaabaaabbbbbabaabbbbbbbbabbbabbabbbbabbbbabbbbbbabababbaaaabbbbaabaaababbbabaaaabaabbbabbaabbabbbbabaababbbbbbbabbaaaabaaabbaaabaaaaababbbaaaabbbbbabbabb" ,"ba*ba*bb*a********abaa*bb**abb**b***ab**b*b*babb***a*bb*aaabb*****b*aabb**aa**b*a***b*bb*b*bb*a*bbbbb**")
         True
-        >>> s.isMatch("abcc","a*??")
-        True
-        >>> s.isMatch("aaab", "a*")
-        True
-        >>> s.isMatch("baabbabababaabbabababbaabbbbaaabaaabbbbaaaaaabbbbaaabaaabbbbbabaabbbbbbbbabbbabbabbbbabbbbabbbbbbabababbaaaabbbbaabaaababbbabaaaabaabbbabbaabbabbbbabaababbbbbbbabbaaaabaaabbaaabaaaaababbbaaaabbbbbabbabb", "ba*ba*bb*a********abaa*bb**abb**b***ab**b*b*babb***a*bb*aaabb*****b*aabb**aa**b*a***b*bb*b*bb*a*bbbbb**")
+        >>> s.isMatch("babbbbaabababaabbababaababaabbaabababbaaababbababaaaaaabbabaaaabababbabbababbbaaaababbbabbbbbbbbbbaabbb" ,"b**bb**a**bba*b**a*bbb**aba***babbb*aa****aabb*bbb***a")
         False
         """
         if len(s) == 0:
-            if self.reduce(p) == '':
-                return True
-            else:
-                return False
-
-        if len(p) == 0:
             return False
 
-        for candidate in self.candidates(p):
-            if candidate and (s[0] == candidate[0] or candidate[0] == '?') and self.isMatch(s[1:], candidate[1:]):
-                    return True
+        p = self.optimize(p)
+        return self.judge(0, s, self.transitions(p))
 
-        return False
+
+
 
 
