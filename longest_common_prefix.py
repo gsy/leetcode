392d9a716f10745d1aac0855acd10a3e0b126c0b
xuanguang.chen <gtcxg@hotmail.com>
2015-12-22 00:29:56 +0800

    NO.14 longest common prefix

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,54 +1,6 @@
 __author__ = 'guang'
 
 class Solution(object):
-    def commonPrefix(self, s1, s2):
-        if s1 == '' or s2 == '':
-            return ''
-
-        if s1[0] != s2[0]:
-            return ''
-
-        return s1[0] + self.commonPrefix(s1[1:], s2[1:])
-
-    def longestStr(self, strs):
-        lengths = map(len, strs)
-        max = 0
-        result = 0
-        for i, length in enumerate(lengths):
-            if length > max:
-                max = length
-                result = i
-
-        return result
-
-    def foo(self, strs):
-        if len(strs) <= 1:
-            return ''
-
-        result = ''
-        max = 0
-        i = self.longestStr(strs)
-        x = strs[i]
-        new_strs = []
-        for j in range(len(strs)):
-            if j == i:
-                continue
-            y = strs[j]
-            prefix = self.commonPrefix(x, y)
-            length = len(prefix)
-            if length == 0:
-                new_strs.append(y)
-            elif length > max:
-                max = length
-                result = prefix
-
-        new_prefix = self.foo(new_strs)
-        if len(result) > len(new_prefix):
-            return result
-        else:
-            return new_prefix
-
-
     def longestCommonPrefix(self, strs):
         """
         :type strs: List[str]
@@ -61,13 +13,13 @@ class Solution(object):
         >>> s.longestCommonPrefix([""])
         ''
         >>> s.longestCommonPrefix(["a", "ab", "abcd"])
-        'ab'
+        'a'
         >>> s.longestCommonPrefix(["abc", "ad", "bcd"])
-        'a'
+        ''
         >>> s.longestCommonPrefix(["bcd", "abc", "ad"])
-        'a'
+        ''
         >>> s.longestCommonPrefix(["abcd", "bcdf", "ab", "b"])
-        'ab'
+        ''
         >>> s.longestCommonPrefix(["bcdf", "b"])
         'b'
         >>> s.longestCommonPrefix(["a"])
@@ -75,12 +27,28 @@ class Solution(object):
         >>> s.longestCommonPrefix(["a", "a", "b"])
         ''
         """
+
         if len(strs) == 0:
             return ''
-        elif len(strs) == 1:
-            return strs[0]
 
-        return self.foo(strs)
+        length = 0
+        x = strs[0]
+        for i in range(len(x)):
+            match = True
+            for y in strs[1:]:
+                if i < len(y) and x[i] == y[i]:
+                    continue
+                else:
+                    match = False
+                    break
+            if match:
+                length += 1
+            else:
+                break
+
+        return x[:length]
+
+
 
 
 
