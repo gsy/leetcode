d9b8d31908910e8eda1138ed75180f5b8687f00a
xuanguang.chen <gtcxg@hotmail.com>
2015-12-23 00:20:02 +0800

    No.20 Valid Parentheses

---
+++ valid_parentheses.py
@@ -0,0 +1,49 @@
+__author__ = 'guang'
+
+class Solution(object):
+    def match(self, x, y):
+        return (x, y) in [('(', ')'), ('[', ']'), ('{', '}')]
+
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        >>> s = Solution()
+        >>> s.isValid("{{}}")
+        True
+        >>> s.isValid("[{}])")
+        False
+        >>> s.isValid("(}")
+        False
+        >>> s.isValid("")
+        True
+        >>> s.isValid("([]")
+        False
+        """
+
+        if len(s) == 0:
+            return True
+
+        open = ['(', '[', '{']
+        close = [')', ']', '}']
+
+        if s[-1] in open:
+            return False
+
+        stack = []
+        for x in s:
+            if x in open:
+                stack.append(x)
+            else:
+                if not stack:
+                    return False
+                else:
+                    y = stack.pop()
+                    if not self.match(y, x):
+                        return False
+
+        return len(stack) == 0
+
+
+
+
