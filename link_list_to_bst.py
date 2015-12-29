3a5f8cc392a6d163cb52eb2dd26c1c20586363d7
xuanguang.chen <gtcxg@hotmail.com>
2015-12-29 14:22:58 +0800

    link list to bst, recursive

--- link_list_to_bst.py
+++ link_list_to_bst.py
@@ -5,6 +5,21 @@ from bst import TreeNode, BST
 
 class Solution(object):
 
+    def length(self, ls):
+        """
+        >>> s = Solution()
+        >>> ls = LinkedList.fromList([1,2,3])
+        >>> s.length(ls)
+        3
+        """
+        count = 0
+        node = ls
+        while node:
+            count += 1
+            node = node.next
+
+        return count
+
     def is_leaf(self, tree):
         return tree and tree.left is None and tree.right is None
 
@@ -66,6 +81,74 @@ class Solution(object):
         else:
             return 1 + max(self.height(tree.left), self.height(tree.right))
 
+    def list_to_BST_helper(self, node, start, end):
+        """
+        >>> s = Solution()
+        >>> node = LinkedList.fromList([4, 5])
+        >>> tree, head = s.list_to_BST_helper(node, 4, 5)
+        >>> tree.val
+        5
+        >>> tree.left.val
+        4
+        >>> head.val
+        5
+        >>> node = LinkedList.fromList([1, 2])
+        >>> tree, head = s.list_to_BST_helper(node, 1, 2)
+        >>> tree.val
+        2
+        >>> tree.left.val
+        1
+        >>> head.val
+        2
+        >>> node = LinkedList.fromList([1, 2, 3, 4, 5])
+        >>> tree, head = s.list_to_BST_helper(node, 1, 5)
+        >>> head.val
+        5
+
+        # >>> BST(tree).show()
+
+        >>> node = LinkedList.fromList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        >>> tree, head = s.list_to_BST_helper(node, 1, 15)
+        >>> head.val
+        15
+        >>> BST(tree).show()
+
+
+        """
+        length = end - start + 1
+
+        if length <= 0 or node is None:
+            return None, node
+
+        if length == 1:
+            tree = TreeNode(node.val)
+            return tree, node
+        elif length == 2:
+            left = TreeNode(node.val)
+            node = node.next
+            root = TreeNode(node.val)
+            root.left = left
+            return root, node
+        elif length == 3:
+            left = TreeNode(node.val)
+            node = node.next
+            root = TreeNode(node.val)
+            node = node.next
+            right = TreeNode(node.val)
+            root.left = left
+            root.right = right
+            return root, node
+
+        middle = start + length / 2
+        left, head = self.list_to_BST_helper(node, start, middle - 1)
+        head = head.next
+        root = TreeNode(head.val)
+        root.left = left
+        head = head.next
+        right, new_head = self.list_to_BST_helper(head, middle + 1, end)
+        root.right = right
+        return root, new_head
+
     def sortedListToBST(self, head):
         """
         :type head: ListNode
@@ -73,26 +156,26 @@ class Solution(object):
         >>> s = Solution()
         >>> head = LinkedList.fromList([1, 2, 3])
         >>> result = s.sortedListToBST(head)
-        >>> tree = BST(result)
-        >>> tree.show()
-
-        >>> head = LinkedList.fromList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        >>> result.val
+        2
+        >>> head = LinkedList.fromList([1, 2])
         >>> result = s.sortedListToBST(head)
-        >>> tree = BST(result)
-        >>> tree.show()
-        >>> head = LinkedList
+        >>> result.val
+        2
+        >>> result.left.val
+        1
+        >>> result.right
 
+        >>> head = LinkedList.fromList([1])
+        >>> result = s.sortedListToBST(head)
+        >>> result.val == 1 and result.left is None and result.right is None
+        True
         """
         if head is None:
             return None
 
-        node = head
-        tree = None
-        count = 0
-        while node:
-            tree = self.cons(node.val, tree, count)
-            node = node.next
-            count += 1
+        length = self.length(head)
+        return self.list_to_BST_helper(head, 1, length)[0]
 
-        return tree
+
 
